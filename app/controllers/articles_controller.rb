1f148bc5581db7bfb5010cfea85959b593d910c7
Anil Kulkarni <anil.kulkarni@outlook.com>
2017-10-16 14:21:31 -0700

    Add a new form

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,4 +3,14 @@ class ArticlesController < ApplicationController
     @articles = Article.all
     render :index
   end
+
+  def new
+    @article = Article.new()
+    render :new
+  end
+
+  def show
+    @article = Article.find(params[:id])
+    render :show
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
-  resources :articles, only: :index
+  resources :articles, only: [:index, :show, :new]
+  root "articles#index"
 end
